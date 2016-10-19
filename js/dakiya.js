f09a51da1a0c48e709a3374cd2e50d05cd72917d
rupeshmore <rupesh.more@fairfaxmedia.co.nz>
2016-10-19 15:21:04 +1300

    initial commit

--- js/dakiya.js
+++ js/dakiya.js
@@ -17,15 +17,6 @@ SUCH DAMAGE.
 // convert test scripts to validations.
 // add mocha tests
 
-//Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-85613401-2', 'auto');
-ga('send', 'pageview');
-
 let scriptObject, envCollection, collection, fileName, dakiyaItems, tool, collectionName, artilleryTarget;
 
 window.onload = function() {
